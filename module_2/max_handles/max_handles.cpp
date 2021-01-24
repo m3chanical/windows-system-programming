4c3d5b936a389c913cb50a88976a66e3ddf1f93c
sinbeard <sinbeard@protonmail.com>
2021-01-23 23:49:33 -0500

    add (some of) module 2 code

--- module_2/max_handles/max_handles.cpp
+++ module_2/max_handles/max_handles.cpp
@@ -1,6 +1,44 @@
+#include <Windows.h>
+#include <stdio.h>
 
-
-int main (int argc, char* argv[])
+int main (void)
 {
+	UINT32 handle_count = 0;
+	HANDLE mut = ::CreateMutex(nullptr, true, nullptr);
+	HANDLE mutex_dup;
+	if (!mut) // if createmutex fails, return is null
+		return -1; 
+
+	/* CREATE JOB OBJECT TEST */
+
+	//while(::CreateJobObjectW(nullptr, nullptr))
+	//{
+	//	handle_count++;
+	//}
 	
+	/* DUPLICATE HANDLE TEST */
+
+	//while(DuplicateHandle(GetCurrentProcess(), 
+	//						mut, 
+	//						GetCurrentProcess(),
+	//						&mutex_dup,
+	//						0,
+	//						FALSE,
+	//						DUPLICATE_SAME_ACCESS))
+	//{
+	//	handle_count++;
+	//}
+
+	// Duplicate Handle also gets up to 16 mil.
+
+	/* MAX HANDLE TEST */
+	
+	//while(::CreateMutex(nullptr, true, nullptr))
+	//{
+	//	handle_count++;
+	//}
+	//// should get around 16 mil handles
+	
+	printf("CreateMutex failed at %d handles. Last Error: %lu\n", handle_count, GetLastError());
+	return 0;
 }
--- module_2/single/single.cpp
+++ module_2/single/single.cpp
@@ -1,20 +1,57 @@
-// single.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include <iostream>
+#include <Windows.h>
+#include <tchar.h>
+#include <Psapi.h>
+#include <stdio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	HANDLE mut = ::CreateMutex(nullptr, true, TEXT("SingleInstance"));
+	if (mut == nullptr)
+		return GetLastError();
+
+	if(GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		// already exists... exit
+	}
+
+	// do cool stuff.
+
+
+/*
+	// random code notes:
+	// standard C...
+	char name[] = "notepad";
+	wchar_t wname[] = L"notepad"; // 2-byte character. also need to add the prefix L to indicate to the compiler that it needs to create a utf16 str
+
+	//what about either? can use preprocessor macros --> "ifdef UNICODE ... "
+	// this also works:
+	TCHAR tname[] = _T("notepad");
+	WCHAR wname_2[] = L"notepad"; 
+
+
+	wchar_t path[MAX_PATH];
+	::GetSystemDirectory(path, _countof(path)); // _countof --> sizeof(x) / sizeof(x[0])
+
+	TCHAR pc_name[MAX_COMPUTERNAME_LENGTH + 1];
+	DWORD size = _countof(pc_name);
+	::GetComputerName(pc_name, &size); // if true, success. --> _Out_ parameters must be pointers
+
+	// deprecated:
+	OSVERSIONINFO osvi;
+	osvi.dwOSVersionInfoSize = sizeof(osvi);
+	::GetVersionEx(&osvi);
+	
+	PERFORMANCE_INFORMATION pi;
+	for(;;)
+	{
+		::GetPerformanceInfo(&pi, sizeof(pi));
+		printf("Threads: %u Handles: %u Committed: %zu MB\n",
+			pi.ThreadCount, pi.HandleCount, pi.CommitTotal >> 8); // bytes, to MB 
+		::Sleep(1000);
+	}
+	
+*/
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
